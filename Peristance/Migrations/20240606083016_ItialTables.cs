2ef4d88a7b8cf28be474c0b52e587085effb1e47
Dell <Dell@DESKTOP-K7T63BU>
2024-06-07 10:57:56 +0530

    Changes in appsettings.json

--- Peristance/Migrations/20240606083016_ItialTables.cs
+++ Peristance/Migrations/20240606083016_ItialTables.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 namespace Peristance.Migrations
 {
     /// <inheritdoc />
-    public partial class AddCityTable : Migration
+    public partial class ItialTables : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
